4c08eb2032fcfa88bacffe465b4b9d3fb897eea5
Nina Mutty <ninamutty@gmail.com>
2016-11-29 19:22:15 -0800

    information on modal - need to hide it

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,28 +1,39 @@
-import $ from 'jquery';    // Letting us use jquery within this document
-import Backbone from 'backbone'; // importing backbone
-import _ from 'underscore';
-import Contact from 'app/models/contact';
-import RolodexView from 'app/views/rolodex_view';
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
-import Rolodex from 'app/collections/rolodex';
-
+import $ from 'jquery';
 
 const ContactView = Backbone.View.extend({
   initialize: function(options) {
     // this.model = options.contact;
-    this.template = options.template;
+    this.contactTemplate = options.contactTemplate;
+    this.modalTemplate = options.modalTemplate;
     // this.listenTo(this.model, 'change', this.render);
   },
 
   render: function() {
     this.delegateEvents();
-    var html = this.template(this.model.toJSON());
+    var html = this.contactTemplate(this.model.toJSON());
     this.$el.html(html);
 
     return this;
+  },
+
+  events: {
+    'click .contact-card': 'showModal',
+    'click document': 'hideModal'
+  },
+
+  showModal: function() {
+    var html = this.modalTemplate(this.model.toJSON());
+    $('#contact-details').html(html).show();
+  },
+
+
+  hideModal: function() {
+    console.log("hide");
+    if ($('#contact-details').is(':visible')) {
+      $('#contact-details').hide();
+    }
   }
 
-});
+}); // end ContactView
 
 export default ContactView;
